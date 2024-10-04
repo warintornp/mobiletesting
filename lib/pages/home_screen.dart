43fe48ba321f380f7596ba3e110af1f9000b4afb
warintornp <49806900+warintornp@users.noreply.github.com>
2024-10-04 22:17:48 +0700

    Refactor Home page

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobiletesting/pages/login_with_pin_screen.dart';
 import 'package:mobiletesting/pages/news_screen.dart';
 import 'package:mobiletesting/pages/profile_screen.dart';
 
@@ -7,14 +6,6 @@ class HomeScreen extends StatelessWidget {
   final Map<String, dynamic> userDetails;
 
   HomeScreen({required this.userDetails});
-
-// List of news images
-  final List<String> newsImages = [
-    'https://via.placeholder.com/600x400.png?text=News+1',
-    'https://via.placeholder.com/600x400.png?text=News+2',
-    'https://via.placeholder.com/600x400.png?text=News+3',
-    'https://via.placeholder.com/600x400.png?text=News+4',
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,97 +20,15 @@ class HomeScreen extends StatelessWidget {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Stack(
-                    children: [
-                      // Background gradient container
-                      Container(
-                        height: 100,
-                        width: double.maxFinite,
-                        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.blueAccent, Colors.lightBlueAccent],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                        ),
-                      ),
-                      Positioned(
-                        top: 20,
-                        left: 0,
-                        right: 0,
-                        child: Column(
-                          children: [
-                            // User Name
-                            Text(
-                              userDetails['name'],
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            // User Email
-                            Text(
-                              userDetails['email'],
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.white70,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  //User profile
+                  UserProfileWidget(
+                    userDetails: userDetails,
                   ),
                   const SizedBox(height: 150),
                   //News Section Header
-                  const Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Latest News',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ),
+                  newsheader(),
                   // Image Carousel
-                  Container(
-                    height: 150.0, // Set the height of the carousel
-                    width: 70.0,
-                    child: PageView.builder(
-                      itemCount: newsImages.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            // Navigate to the DetailsPage
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    NewsScreen(imageURL: newsImages[index]),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            margin: EdgeInsets.symmetric(horizontal: 8.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                  15.0), // Adjust radius here
-                              child: Image.network(
-                                newsImages[index],
-                                fit: BoxFit.cover,
-                                width: double
-                                    .infinity, // Stretch image to fill width
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  ImageCarousel(),
                 ]),
           ),
         ],
@@ -151,6 +60,117 @@ class HomeScreen extends StatelessWidget {
           ),
         ]),
       ),
+    );
+  }
+
+  Widget newsheader() {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Text(
+        'Latest News',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
+  }
+}
+
+class ImageCarousel extends StatelessWidget {
+  // List of news images
+  final List<String> newsImages = [
+    // 'https://via.placeholder.com/600x400.png?text=News+1',
+    // 'https://via.placeholder.com/600x400.png?text=News+2',
+    // 'https://via.placeholder.com/600x400.png?text=News+3',
+    // 'https://via.placeholder.com/600x400.png?text=News+4',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150.0, // Set the height of the carousel
+      width: 70.0,
+      child: PageView.builder(
+        itemCount: newsImages.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // Navigate to the DetailsPage
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewsScreen(imageURL: newsImages[index]),
+                ),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0), // Adjust radius here
+                child: Image.network(
+                  newsImages[index],
+                  fit: BoxFit.cover,
+                  width: double.infinity, // Stretch image to fill width
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Background gradient container
+class UserProfileWidget extends StatelessWidget {
+  final Map<String, dynamic> userDetails;
+  const UserProfileWidget({super.key, required this.userDetails});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 100,
+          width: double.maxFinite,
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.lightBlueAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+        ),
+        Positioned(
+          top: 20,
+          left: 0,
+          right: 0,
+          child: Column(
+            children: [
+              // User Name
+              Text(
+                userDetails['name'],
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              // User Email
+              Text(
+                userDetails['email'],
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
