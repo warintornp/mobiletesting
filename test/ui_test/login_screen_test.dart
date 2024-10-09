66437737a1cb407fb162f3f651ca2c0c2468d1d9
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-09 17:07:55 +0700

    add widget test case

--- test/ui_test/login_screen_test.dart
+++ test/ui_test/login_screen_test.dart
@@ -22,12 +22,15 @@ void main() {
     mockUserService = MockUserService();
   });
 
-  //Need P'Ann Assist - fixed
-  testWidgets('Valid PIN and successful API call with dialog',
+  testWidgets('successful validate PIN both FE and BE',
       (WidgetTester tester) async {
-    // Mock user service responses
-    when(mockUserService.fetchUserDetails2('132495'))
-        .thenAnswer((_) async => null);
+    // when(mockUserService.fetchUserDetails2('132495'))
+    //     .thenAnswer((_) async => null);
+
+    when(mockUserService.fetchUserDetails2('132495')).thenAnswer((_) async => {
+          'name': 'John Doe',
+          'email': 'john.doe@example.com',
+        });
 
     //Set screen size, if not will show default size which not cover the page
     final TestWidgetsFlutterBinding binding =
@@ -52,16 +55,10 @@ void main() {
     await tester.tap(find.text('9'));
     await tester.tap(find.text('5'));
     await tester.pumpAndSettle();
+
     //Assertion
-
-    //Need P'Ann Assist - done
-    expect(find.byType(AlertDialog),
-        findsOneWidget); //Currently after BE validated PIN success -> we display the alert popup - need to be fix
-    expect(find.text('Invalid pin or network issue. please try again.'),
-        findsOneWidget);
-
-    //Verify mock service should get call becuase FE validate passed
-    verify(mockUserService.fetchUserDetails2('132495')).called(1); //Failed here
+    expect(find.byType(AlertDialog), findsNothing);
+    verify(mockUserService.fetchUserDetails2('132495')).called(1);
   }, tags: 'widget');
 
   testWidgets('Invalid (FE rule) PIN with dialog', (WidgetTester tester) async {
@@ -88,7 +85,38 @@ void main() {
     expect(find.text('PIN must not have repeating digits'), findsOneWidget);
   }, tags: 'widget');
 
-  testWidgets(
-      'Invalid (BE rule) PIN with dialog', (WidgetTester tester) async {},
-      tags: 'widget');
+  testWidgets('Invalid (BE rule) PIN with dialog', (WidgetTester tester) async {
+    when(mockUserService.fetchUserDetails2('132495'))
+        .thenAnswer((_) async => null);
+
+    //Set screen size, if not will show default size which not cover the page
+    final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(Size(400, 800));
+
+    //Create widget
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider(
+          create: (_) => LoginViewModel(mockUserService, SortOrder.ascending),
+          child: const LoginScreen(),
+        ),
+      ),
+    );
+
+    //Action tap on numpad
+    await tester.tap(find.text('1'));
+    await tester.tap(find.text('3'));
+    await tester.tap(find.text('2'));
+    await tester.tap(find.text('4'));
+    await tester.tap(find.text('9'));
+    await tester.tap(find.text('5'));
+    await tester.pumpAndSettle();
+
+    //Assertion
+    expect(find.byType(AlertDialog), findsOneWidget);
+    expect(find.text('Invalid pin or network issue. please try again.'),
+        findsOneWidget);
+    verify(mockUserService.fetchUserDetails2('132495')).called(1);
+  }, tags: 'widget');
 }
