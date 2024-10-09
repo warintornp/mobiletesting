3a3790f6e88deb11b623a42daf7e81d50b873e49
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-09 16:56:57 +0700

    fix widget testing

--- test/ui_test/login_screen_test.dart
+++ test/ui_test/login_screen_test.dart
@@ -22,45 +22,47 @@ void main() {
     mockUserService = MockUserService();
   });
 
-  //Need P'Ann Assist - Event we mock userService but it will go through real code T^T
-  // testWidgets('Valid PIN and successful API call with dialog',
-  //     (WidgetTester tester) async {
-  //   // Mock user service responses
-  //   when(mockUserService.fetchUserDetails2('132495')).thenAnswer((_) async => null);
+  //Need P'Ann Assist - fixed
+  testWidgets('Valid PIN and successful API call with dialog',
+      (WidgetTester tester) async {
+    // Mock user service responses
+    when(mockUserService.fetchUserDetails2('132495'))
+        .thenAnswer((_) async => null);
 
-  //   //Set screen size, if not will show default size which not cover the page
-  //   final TestWidgetsFlutterBinding binding =
-  //       TestWidgetsFlutterBinding.ensureInitialized();
-  //   await binding.setSurfaceSize(Size(400, 800));
+    //Set screen size, if not will show default size which not cover the page
+    final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(Size(400, 800));
 
-  //   //Create widget
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: ChangeNotifierProvider(
-  //         create: (_) => LoginViewModel(mockUserService, SortOrder.ascending),
-  //         child: const LoginScreen(),
-  //       ),
-  //     ),
-  //   );
+    //Create widget
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider(
+          create: (_) => LoginViewModel(mockUserService, SortOrder.ascending),
+          child: const LoginScreen(),
+        ),
+      ),
+    );
 
-  //   //Action tap on numpad
-  //   await tester.tap(find.text('1'));
-  //   await tester.tap(find.text('3'));
-  //   await tester.tap(find.text('2'));
-  //   await tester.tap(find.text('4'));
-  //   await tester.tap(find.text('9'));
-  //   await tester.tap(find.text('5'));
-  //   await tester.pumpAndSettle();
-  //   //Assertion 
+    //Action tap on numpad
+    await tester.tap(find.text('1'));
+    await tester.tap(find.text('3'));
+    await tester.tap(find.text('2'));
+    await tester.tap(find.text('4'));
+    await tester.tap(find.text('9'));
+    await tester.tap(find.text('5'));
+    await tester.pumpAndSettle();
+    //Assertion
 
-  //   //Need P'Ann Assist 
-  //   expect(find.byType(AlertDialog), findsOneWidget); //Currently after BE validated PIN success -> we display the alert popup - need to be fix
-  //   expect(find.text('Invalid pin or network issue. please try again.'), findsOneWidget);
+    //Need P'Ann Assist - done
+    expect(find.byType(AlertDialog),
+        findsOneWidget); //Currently after BE validated PIN success -> we display the alert popup - need to be fix
+    expect(find.text('Invalid pin or network issue. please try again.'),
+        findsOneWidget);
 
-  //   //Verify mock service should get call becuase FE validate passed 
-  //   verify(mockUserService.fetchUserDetails2('132495')).called(1); //Failed here 
-
-  // }, tags: 'widget');
+    //Verify mock service should get call becuase FE validate passed
+    verify(mockUserService.fetchUserDetails2('132495')).called(1); //Failed here
+  }, tags: 'widget');
 
   testWidgets('Invalid (FE rule) PIN with dialog', (WidgetTester tester) async {
     final TestWidgetsFlutterBinding binding =
