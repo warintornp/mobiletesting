fc28c238ef0a3313fac65162b5c16b1341c2899b
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-09 21:24:00 +0700

    extract Dot view

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -2,6 +2,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:mobiletesting/pages/dot_view.dart';
 import 'package:mobiletesting/pages/login_view_model.dart';
 import 'package:mobiletesting/pages/pin_grid_view.dart';
 import 'package:mobiletesting/pages/sort_order.dart';
@@ -49,42 +50,6 @@ class LoginScreen extends StatelessWidget {
           deleteButtonOnPressed: viewModel.onDeleteButtonPressed,
           numberButtonOnPressed: (pressedDigit) =>
               {viewModel.onDigitPressed(pressedDigit, context)}),
-    );
-  }
-}
-
-class Dot extends StatelessWidget {
-  final LoginViewModel viewModel;
-  const Dot({super.key, required this.viewModel});
-
-  @override
-  Widget build(Object context) {
-    return Container(
-      padding: const EdgeInsets.all(30),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        ...List<int>.generate(6, (i) => i + 1).map<Widget>(
-          (number) => createCircle(number, viewModel),
-        )
-      ]),
-    );
-  }
-
-  Widget createCircle(int index, LoginViewModel viewModel) {
-    return Container(
-      width: 15.0,
-      height: 15.0,
-      decoration: index <= viewModel.inputtedPin.length
-          ? const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.orange,
-            )
-          : BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.orange,
-                width: 2.0,
-              ),
-            ),
     );
   }
 }
