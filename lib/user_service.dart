52dbcdddd610f09185ccc4b6ddf17b8ef164f23a
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-09 17:37:26 +0700

    unused file

--- lib/user_service.dart
+++ lib/user_service.dart
@@ -2,21 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class UserService {
-  final String _apiUrl = 'http://localhost:3000/getUserDetails';
-
-  Future<Map<String, dynamic>?> fetchUserDetails(String pin) async {
-    final url =
-        Uri.parse('$_apiUrl?pin=$pin'); // Use GET request with query parameters
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      // print('Failed to fetch user details: ${response.statusCode}');
-      return null;
-    }
-  }
-
   Future<Map<String, dynamic>?> fetchUserDetails2(String pin) async {
     final url = Uri.parse('http://localhost:3000/v1/api/pin/validate');
     final body = jsonEncode({'pin': pin});
