cf23606bee806a1e8932ef7c75232d94f31df312
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-11 22:55:44 +0700

    create new endpoint, /v1/api/user/tier

--- api/server2/app.js
+++ api/server2/app.js
@@ -9,10 +9,14 @@ const points = {
   'rr4RIu-hZkeryZRXK7dEwquQn4rVGqZFEmKWwuR_kYs': 300,
 }
 
+const tiers = {
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9: 'bronze',
+  eyJlbWFpbCI6InBhdEBleGFtcGxlLmNvbSJ9: 'silver',
+  'rr4RIu-hZkeryZRXK7dEwquQn4rVGqZFEmKWwuR_kYs': 'gold',
+}
+
 app.use(bodyParser.json())
-app.get('/v1/api/user/tier', (req, res) => {
-  res.status(200).json('')
-})
+
 app.get('/v1/api/user/point', (req, res) => {
   if (req.headers.authorization == undefined) {
     res.status(401).json({ error: 'Unauthorised' })
@@ -27,6 +31,23 @@ app.get('/v1/api/user/point', (req, res) => {
   } else {
     res.setHeader('Authorization', req.headers.authorization)
     res.status(200).json({ point: points[authToken] })
+  }
+})
+
+app.get('/v1/api/user/tier', (req, res) => {
+  if (req.headers.authorization == undefined) {
+    res.status(401).json({ error: 'Unauthorised' })
+    return
+  }
+
+  const authToken = req.headers.authorization.split(' ')[1]
+  if (authToken === undefined || authToken === '') {
+    res.status(400).json({ error: 'Invalid request' })
+  } else if (tiers[authToken] === undefined) {
+    res.status(401).json({ error: 'Unauthorised' })
+  } else {
+    res.setHeader('Authorization', req.headers.authorization)
+    res.status(200).json({ tier: 'bronze' })
   }
 })
 
--- api/server2/app.test.js
+++ api/server2/app.test.js
@@ -64,4 +64,66 @@ describe('GET user point', () => {
     done()
   })
 })
-describe('GET user tier', () => {})
+describe('GET user tier', () => {
+  it('200', async () => {
+    // Act
+    const response = await request(app)
+      .get('/v1/api/user/tier')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
+
+    // Assert
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      tier: 'bronze',
+    })
+    expect(response.header.authorization).toEqual(
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+    )
+  })
+  it('401 Unauthorised WHEN authToken is invalid token', async () => {
+    // Act
+    const response = await request(app)
+      .get('/v1/api/user/tier')
+      .set('Authorization', 'Bearer token')
+
+    // Assert
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({ error: 'Unauthorised' })
+    expect(response.header.authorization).toBeUndefined()
+  })
+  it('400  WHEN authToken is invalid format', async () => {
+    // Act
+    const response = await request(app)
+      .get('/v1/api/user/tier')
+      .set('Authorization', 'Bearer11111')
+
+    // Assert
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({ error: 'Invalid request' })
+    expect(response.header.authorization).toBeUndefined()
+  })
+  it('401 Unauthorised WHEN authToken is absent', async () => {
+    // Act
+    const response = await request(app).get('/v1/api/user/tier')
+
+    // Assert
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({ error: 'Unauthorised' })
+    expect(response.header.authorization).toBeUndefined()
+  })
+  it('400 WHEN authToken is empty string', async () => {
+    // Act
+    const response = await request(app)
+      .get('/v1/api/user/tier')
+      .set('Authorization', '')
+
+    // Assert
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({ error: 'Invalid request' })
+    expect(response.header.authorization).toBeUndefined()
+  })
+  afterAll((done) => {
+    app.close()
+    done()
+  })
+})
