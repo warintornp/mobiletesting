e1a351d37095f53386851349c99d1bbf99dd83c6
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-11 23:43:51 +0700

    1st thin slice: server1 & 2

--- api/server1/app.js
+++ api/server1/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const http = require('node:http')
 const port = 3000
 
 const authToken = {
@@ -51,17 +52,84 @@ app.get('/v1/api/user', (req, res) => {
 
   const authToken = req.headers.authorization.split(' ')[1]
   console.log(`authToken: ${authToken}`)
-  //return http response 401 if authToken is missing.
   if (authToken === undefined || authToken === '') {
     res.status(400).json({ error: 'Invalid request' })
   } else if (users[authToken] === undefined) {
     res.status(401).json({ error: 'Unauthorised' })
   } else {
-    res.setHeader('Authorization', req.headers.authorization)
-    res.status(200).json(users[authToken])
+    getUserData()
+  }
+
+  function getUserData() {
+    Promise.all([
+      fetchUserPoint(req.headers.authorization),
+      fetchUserTier(req.headers.authorization),
+    ])
+      .then(([userPoint, userTier]) => {
+        res.setHeader('Authorization', req.headers.authorization)
+        res.status(200).json({ ...users[authToken], ...userTier, ...userPoint })
+      })
+      .catch((error) => {
+        console.error('Error:', error.message)
+        res.status(500).json({ error: 'Internal Server Error' })
+      })
   }
 })
 
+function fetchUserPoint(authToken) {
+  return new Promise((resolve, reject) => {
+    http
+      .get(
+        'http://localhost:4000/v1/api/user/point',
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        },
+        (response) => {
+          let data = ''
+          response.on('data', (chunk) => {
+            data += chunk
+          })
+          response.on('end', () => {
+            const userPoint = JSON.parse(data)
+            resolve(userPoint)
+          })
+        }
+      )
+      .on('error', (error) => {
+        console.error('Error:', error.message)
+        reject(error)
+      })
+  })
+}
+function fetchUserTier(authToken) {
+  return new Promise((resolve, reject) => {
+    http
+      .get(
+        'http://localhost:4000/v1/api/user/tier',
+        {
+          headers: {
+            Authorization: authToken,
+          },
+        },
+        (response) => {
+          let data = ''
+          response.on('data', (chunk) => {
+            data += chunk
+          })
+          response.on('end', () => {
+            const userTier = JSON.parse(data)
+            resolve(userTier)
+          })
+        }
+      )
+      .on('error', (error) => {
+        console.error('Error:', error.message)
+        reject(error)
+      })
+  })
+}
 var server = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
