5a65db0555302e1263e58533f67cc7a77530fbb0
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-22 20:26:38 +0700

    complete home view model test

--- lib/pages/home_view_model.dart
+++ lib/pages/home_view_model.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:mobiletesting/user_service.dart';
 
 class HomeViewModel extends ChangeNotifier {
+  final UserService userService;
   Map<String, dynamic> _userDetails = {};
 
+  HomeViewModel({required this.userService});
   Map<String, dynamic> get userDetails => _userDetails;
 
   Future<void> onDidLoad() async {
-    _userDetails = await UserService().fetchUserDetails();
+    _userDetails = await userService.fetchUserDetails();
     notifyListeners();
   }
 }
--- test/unit_testing/home_view_model_test.dart
+++ test/unit_testing/home_view_model_test.dart
@@ -1,1 +1,59 @@
-// TODO
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobiletesting/pages/home_view_model.dart';
+import 'package:mobiletesting/user_service.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+import 'home_view_model_test.mocks.dart';
+
+@GenerateMocks([UserService])
+void main() {
+  late HomeViewModel homeViewModel;
+  late MockUserService mockUserService;
+
+  setUp(() {
+    mockUserService = MockUserService();
+    homeViewModel = HomeViewModel(userService: mockUserService);
+  });
+
+  group('HomeViewModel', () {
+    test('initial userDetails should be empty', () {
+      expect(homeViewModel.userDetails, isEmpty);
+    });
+
+    test('onDidLoad should fetch user details and update userDetails',
+        () async {
+      // Arrange
+      final mockUserDetails = {'name': 'John Doe', 'age': 30};
+      when(mockUserService.fetchUserDetails())
+          .thenAnswer((_) async => mockUserDetails);
+
+      // Act
+      await homeViewModel.onDidLoad();
+
+      // Assert
+      expect(homeViewModel.userDetails, equals(mockUserDetails));
+      verify(mockUserService.fetchUserDetails()).called(1);
+    });
+
+    test('onDidLoad should notify listeners', () async {
+      // Arrange
+
+      when(mockUserService.fetchUserDetails()).thenAnswer((_) async => {});
+
+      var listenerCalled = false;
+
+      homeViewModel.addListener(() {
+        listenerCalled = true;
+      });
+
+      // Act
+
+      await homeViewModel.onDidLoad();
+
+      // Assert
+
+      expect(listenerCalled, isTrue);
+    });
+  });
+}
