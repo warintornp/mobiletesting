488d3a027501dd749d866fa362f960cd586658cc
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-24 19:24:17 +0700

    workshop #3 AC#1 increasing case

--- lib/login_screen/pin_rules.dart
+++ lib/login_screen/pin_rules.dart
@@ -1,12 +1,32 @@
 import 'dart:math';
 
-class PinRule {}
+class PinRule {
+  final bool Function(String pin) condition;
+  final String description;
+
+  PinRule(this.condition, this.description);
+}
 
 // workshop #3
 class PinRules {
-  final List<PinRule> _rules = [];
+  final List<PinRule> _rules = [
+    PinRule((pin) => !isSequential(pin), "Pin format is invalid"),
+  ];
 
   String? getErrorMessage(String pin) {
-    return null; // possible to return null or error message
+    // possible to return null or error message
+    if (!_rules[0].condition(pin)) {
+      return _rules[0].description;
+    } else {
+      return null;
+    }
+  }
+
+  static bool isSequential(String pin) {
+    if (pin == "012345") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- test/unit_testing/pin_rules_test.dart
+++ test/unit_testing/pin_rules_test.dart
@@ -1,4 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobiletesting/login_screen/pin_rules.dart';
 
-void main() {}
+void main() {
+  test(
+      'Given The customer is entering 6 digit a PIN when the PIN contains sequential digits then error message return',
+      () {
+    final pin1 = "012345";
+    expect(PinRules().getErrorMessage(pin1), "Pin format is invalid");
+
+    // final pin2 = "543210";
+    // expect(PinRules().getErrorMessage(pin2), "Pin format is invalid");
+
+    // final pin3 = "123456";
+    // expect(PinRules().getErrorMessage(pin3), "Pin format is invalid");
+  });
+}
