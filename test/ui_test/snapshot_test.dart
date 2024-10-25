e24a87e3e4a218423349c44e0e30c66a857d671b
warintornp <49806900+warintornp@users.noreply.github.com>
2024-10-26 00:20:48 +0700

    workshop#7 - Dot view

--- test/ui_test/snapshot_test.dart
+++ test/ui_test/snapshot_test.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:mobiletesting/home_screen/home_view_model.dart';
+import 'package:mobiletesting/login_screen/dot_view.dart';
+import 'package:mobiletesting/login_screen/login_screen.dart';
 import 'package:mobiletesting/login_screen/login_view_model.dart';
 import 'package:mobiletesting/login_screen/login_service.dart';
+import 'package:mobiletesting/login_screen/pin_grid_view.dart';
+import 'package:mobiletesting/login_screen/sort_order.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 
-@GenerateMocks([LoginViewModel, LoginService, HomeViewModel])
+import 'snapshot_test.mocks.dart';
+
+@GenerateMocks([LoginViewModel])
 void main() {
+  late MockLoginViewModel mockLoginViewModel;
   setUpAll(() async {
+    mockLoginViewModel = MockLoginViewModel();
+    //Load app font
     await loadAppFonts();
   });
 
@@ -37,5 +48,21 @@ void main() {
     //     surfaceSize: Size(400, 900), textScaleSize: 1.0);
     // // Assert
     // await screenMatchesGolden(tester, 'pin_grid_view_ascending');
+  });
+
+  testGoldens('Login screen - dot unfilled', (WidgetTester tester) async {
+    when(mockLoginViewModel.inputtedPin).thenReturn("");
+    await tester.pumpWidgetBuilder(
+        Dot(viewModel: mockLoginViewModel),
+        surfaceSize: Size(400, 800));
+    await screenMatchesGolden(tester, 'dot_unfilled');
+  });
+
+  testGoldens('Login screen - dot filled', (WidgetTester tester) async {
+    when(mockLoginViewModel.inputtedPin).thenReturn("1234");
+    await tester.pumpWidgetBuilder(
+        Dot(viewModel: mockLoginViewModel),
+        surfaceSize: Size(400, 800));
+    await screenMatchesGolden(tester, 'dot_input_1234');
   });
 }
