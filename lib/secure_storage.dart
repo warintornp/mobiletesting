04c53fa2fa8613f04dc3f94c74f3349a6c672147
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-26 21:24:04 +0700

    add secure_storage test

--- lib/secure_storage.dart
+++ lib/secure_storage.dart
@@ -1,13 +1,18 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SecureStorage {
+  final SharedPreferences? sharedPreferences;
+  SecureStorage({this.sharedPreferences});
+
+  // this function possible to throw an exception
   Future<void> store(String key, String value) async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs = sharedPreferences ?? await SharedPreferences.getInstance();
     await prefs.setString(key, value);
   }
 
+  // this function possible to throw an exception
   Future<String?> retrieve(String key) async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs = sharedPreferences ?? await SharedPreferences.getInstance();
     return prefs.getString(key);
   }
 }
