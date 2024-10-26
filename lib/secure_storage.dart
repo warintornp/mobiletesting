8c088de0d159e4287c8674c5986be27c22f0a0cc
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-26 21:26:20 +0700

    refactor in order to remove singleton issue

--- lib/secure_storage.dart
+++ lib/secure_storage.dart
@@ -1,7 +1,9 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SecureStorage {
-  static SharedPreferences? sharedPreferences;
+  final SharedPreferences? sharedPreferences;
+  SecureStorage({this.sharedPreferences});
+
   // this function possible to throw an exception
   Future<void> store(String key, String value) async {
     final prefs = sharedPreferences ?? await SharedPreferences.getInstance();
