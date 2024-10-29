7873f67847a1b1cfee2ecf49a582bea280120ae0
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-30 01:06:03 +0700

    workshop #11, dsplay user tier

--- lib/home_screen/user_tier.dart
+++ lib/home_screen/user_tier.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
-enum UserTier {
-  gold,
-  silver,
-  bronze,
+enum UserTier { gold, silver, bronze, unknown }
+
+extension TierColorExtension on UserTier {
+  Color get color {
+    switch (this) {
+      case UserTier.gold:
+        return Colors.amber;
+      case UserTier.silver:
+        return Colors.grey;
+      case UserTier.bronze:
+        return Colors.brown;
+      case UserTier.unknown:
+        return Colors.black;
+    }
+  }
 }
 
-// extension TierColorExtension on UserTier {
-//   Color get color {}
-// }
-
-// extension UserTierExtension on UserTier {
-//   static UserTier fromString(String tier) {}
-// }
+extension UserTierExtension on UserTier {
+  static UserTier fromString(String tier) {
+    switch (tier.toLowerCase()) {
+      case 'gold':
+        return UserTier.gold;
+      case 'silver':
+        return UserTier.silver;
+      case 'bronze':
+        return UserTier.bronze;
+      default:
+        return UserTier.unknown;
+    }
+  }
+}
--- test/unit_testing/user_tier_test.dart
+++ test/unit_testing/user_tier_test.dart
@@ -1,12 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobiletesting/home_screen/user_tier.dart';
 
 void main() {
-  test('example', () {
-    // Arrange
+  group('TierColorExtension', () {
+    test('Gold tier should return amber color', () {
+      expect(UserTier.gold.color, equals(Colors.amber));
+    });
 
-    // Act
+    test('Silver tier should return grey color', () {
+      expect(UserTier.silver.color, equals(Colors.grey));
+    });
 
-    // Assert
-    // expect(moduleA.variableB, '12');
-  }, tags: 'unit');
+    test('Bronze tier should return brown color', () {
+      expect(UserTier.bronze.color, equals(Colors.brown));
+    });
+  });
+
+  group('UserTierExtension', () {
+    test('fromString should return UserTier.gold for "gold"', () {
+      expect(UserTierExtension.fromString('gold'), equals(UserTier.gold));
+    });
+
+    test('fromString should return UserTier.silver for "silver"', () {
+      expect(UserTierExtension.fromString('silver'), equals(UserTier.silver));
+    });
+
+    test('fromString should return UserTier.bronze for "bronze"', () {
+      expect(UserTierExtension.fromString('bronze'), equals(UserTier.bronze));
+    });
+
+    test('fromString should be case-insensitive', () {
+      expect(UserTierExtension.fromString('GOLD'), equals(UserTier.gold));
+      expect(UserTierExtension.fromString('Silver'), equals(UserTier.silver));
+      expect(UserTierExtension.fromString('brONzE'), equals(UserTier.bronze));
+    });
+
+    test('fromString should return UserTier.bronze for unknown string', () {
+      expect(UserTierExtension.fromString('unknown'), equals(UserTier.bronze));
+    });
+  });
 }
