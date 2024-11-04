290d6f31a6d8f620197e87a80b012b3bf23fab7c
warintornp <49806900+warintornp@users.noreply.github.com>
2024-11-05 02:31:18 +0700

    workshop #4 widget test v2

--- test/ui_test/login_screen_test.dart
+++ test/ui_test/login_screen_test.dart
@@ -276,4 +276,55 @@ void main() {
     // await expectLater(
     //     find.byType(MaterialApp), matchesGoldenFile('goldens/widget.png'));
   });
+
+
+  testWidgets('display dialog - v2', (WidgetTester tester) async {
+    //Mock
+    when(mockLoginViewModel.dialogMessage).thenReturn("Test");
+    when(mockLoginViewModel.inputtedPin).thenReturn("132457");
+    when(mockLoginViewModel.isLoading).thenReturn(false);
+    when(mockLoginViewModel.keyPadsortOrder).thenReturn(SortOrder.ascending);
+    //Set screen size before run test
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(Size(400, 800));
+    //Create widget
+    await tester.pumpWidget(
+      MaterialApp(
+          home: ChangeNotifierProvider<LoginViewModel>.value(
+            value:  mockLoginViewModel,
+            child: LoginScreen(),
+      )),
+    );
+    //Act
+    await tester.pumpAndSettle();
+    // //Assert
+    // expect(find.byType(AlertDialog), findsOneWidget);
+    // await expectLater(
+    //     find.byType(MaterialApp), matchesGoldenFile('goldens/widget_v2-1.png'));
+  });
+
+  testWidgets('display text - v2', (WidgetTester tester) async {
+    //Mock
+    when(mockLoginViewModel.dialogMessage).thenReturn("");
+    when(mockLoginViewModel.inputtedPin).thenReturn("132457");
+    when(mockLoginViewModel.isLoading).thenReturn(false);
+    when(mockLoginViewModel.keyPadsortOrder).thenReturn(SortOrder.ascending);
+    //Set screen size before run test
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(Size(400, 800));
+    //Create widget
+    await tester.pumpWidget(
+      MaterialApp(
+          home: ChangeNotifierProvider<LoginViewModel>.value(
+            value:  mockLoginViewModel,
+            child: LoginScreen(),
+      )),
+    );
+    //Act
+    await tester.pumpAndSettle();
+    //Assert
+    // expect(find.text("132457"), findsOneWidget);
+    // await expectLater(
+    //     find.byType(MaterialApp), matchesGoldenFile('goldens/widget_v2-2.png'));
+  });
 }
